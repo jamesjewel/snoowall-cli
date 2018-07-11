4af4ba551a6599051e5fcaccf83974689a59d621
Jewel James <jeweljames11@gmail.com>
2018-07-11 17:03:53 +0530

    Wallpaper setting with library

--- main.go
+++ main.go
@@ -13,13 +13,15 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/reujab/wallpaper"
 	"github.com/turnage/graw/reddit"
 )
 
 var loc = "/home/pro/Dropbox/Code/golang/snoowall/Wallpapers/"
+var datafile = "data"
 var name = "info.agent"
 var path = fmt.Sprintf("%s%s", loc, name)
-var subreddit = "sexy"
+var subreddit = "EarthPorn"
 
 func saveWall(b []byte) error {
 	timestamp := time.Now()
@@ -31,28 +33,50 @@ func saveWall(b []byte) error {
 	return err
 }
 
+func setWall(file string) error {
+	background, err := wallpaper.Get()
+	if err != nil {
+		fmt.Println("[DEBUG] Can't find previous wallpaper:", err)
+	}
+	fmt.Println("Current wallpaper:", background)
+	err = wallpaper.SetFromURL("https://i.imgur.com/pIwrYeM.jpg")
+	if err == nil {
+		fmt.Println("Wallpaper set!")
+	}
+	return err
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	rate := 5 * time.Second
+	var after string
 	script, err := reddit.NewScript("graw:snoowall:0.3.1 by /u/psychemerchant", rate)
 	if err != nil {
 		fmt.Println("[DEBUG] Failed to create script handle: ", err)
 		return
 	}
-	m := make(map[string]string, 0)
-	m["Content-type"] = "image/jpeg"
-	harvest, err := script.ListingWithParams(fmt.Sprintf("/r/%s", subreddit), m)
+	harvest, err := script.Listing(fmt.Sprintf("/r/%s", subreddit), after)
 	if err != nil {
-		fmt.Println("[DEBUG] Failed to fetch /r/:", subreddit, err)
+		fmt.Printf("[DEBUG] Failed to fetch /r/%s: %s", subreddit, err)
 		return
 	}
-	post := harvest.Posts[rand.Intn(30)]
+	post := harvest.Posts[rand.Intn(20)]
+	// str := fmt.Sprintf("Harvest:\n %#v", harvest.Posts[1])
+	// ioutil.WriteFile("harvest", []byte(str), 0600)
+	// bin, _ := ioutil.ReadFile(datafile)
+	// after = string(bin)
+	// fmt.Println("After:", after)
+	// ioutil.WriteFile(datafile, []byte(post.Name), 0600)
 	fmt.Printf("[Title]: %s\n[URL]: %s\n", post.Title, post.URL)
+	// fmt.Printf("[Type]: %s - %s - %s\n", post.Media.OEmbed.Type, post.Media.OEmbed.ProviderName, post.Media.OEmbed.ProviderURL)
+	// fmt.Printf("%+v", post)
+	setWall(post.URL)
 	resp, err := http.Get(post.URL)
-	if err != nil {
+	if err != nil || post.IsRedditMediaDomain == false {
 		fmt.Println("[DEBUG]: Couldn't fetch resource:", post.URL, err)
 		return
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
 	saveWall(body)
+
 }
