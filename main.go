94157a06aacc80ef4a18ffe0e5c2f0a81a2db85f
Jewel James <jeweljames11@gmail.com>
2018-07-11 10:51:48 +0530

    first working prototype

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
 import "fmt"
+import "github.com/turnage/graw/reddit"
+
+var loc = "/home/pro/Dropbox/Code/golang/snoowall/"
+var name = "info.agent"
+var path = fmt.Sprintf("%s%s", loc, name)
+var subreddit = "/r/EarthPorn"
 
 func main() {
 	fmt.Println("Hello Snoowall!")
+	fmt.Println(path)
+	bot, err := reddit.NewBotFromAgentFile(path, 0)
+	fmt.Println(bot, err)
+	harvest, err := bot.Listing(subreddit, "")
+	fmt.Println(harvest, err)
+	if err != nil {
+		fmt.Println("Failed to fetch /r/golang: ", err)
+	}
+	for _, post := range harvest.Posts[:5] {
+		fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
+	}
 }
