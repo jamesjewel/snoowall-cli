07f628986fe44b64085e4877642662e1ea8a879a
Jewel James <jeweljames11@gmail.com>
2018-07-11 14:49:13 +0530

    Added debugging

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-	1. Imgur Integration
+	1. Imgur Integration (Done)
 	2. Wallpaper Setting
 	3. Debugging
 */
@@ -19,24 +19,40 @@ import (
 var loc = "/home/pro/Dropbox/Code/golang/snoowall/Wallpapers/"
 var name = "info.agent"
 var path = fmt.Sprintf("%s%s", loc, name)
-var subreddit = "MapPorn"
+var subreddit = "sexy"
 
-func saveWall(b []byte) {
+func saveWall(b []byte) error {
 	timestamp := time.Now()
 	filename := fmt.Sprintf("%s%s_%s.jpg", loc, subreddit, timestamp.Format("2006-01-02_15-04-05"))
-	fmt.Println("Saving ...")
-	ioutil.WriteFile(filename, b, 0600)
+	err := ioutil.WriteFile(filename, b, 0600)
+	if err == nil {
+		fmt.Println("Saved")
+	}
+	return err
 }
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	rate := 5 * time.Second
-	script, _ := reddit.NewScript("graw:doc_script:0.3.1 by /u/psychemerchant", rate)
-	harvest, _ := script.Listing(fmt.Sprintf("/r/%s", subreddit), "")
+	script, err := reddit.NewScript("graw:snoowall:0.3.1 by /u/psychemerchant", rate)
+	if err != nil {
+		fmt.Println("[DEBUG] Failed to create script handle: ", err)
+		return
+	}
+	m := make(map[string]string, 0)
+	m["Content-type"] = "image/jpeg"
+	harvest, err := script.ListingWithParams(fmt.Sprintf("/r/%s", subreddit), m)
+	if err != nil {
+		fmt.Println("[DEBUG] Failed to fetch /r/:", subreddit, err)
+		return
+	}
 	post := harvest.Posts[rand.Intn(30)]
 	fmt.Printf("[Title]: %s\n[URL]: %s\n", post.Title, post.URL)
-	resp, _ := http.Get(post.URL)
+	resp, err := http.Get(post.URL)
+	if err != nil {
+		fmt.Println("[DEBUG]: Couldn't fetch resource:", post.URL, err)
+		return
+	}
 	body, _ := ioutil.ReadAll(resp.Body)
 	saveWall(body)
-
 }
