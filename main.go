3fae3b99fc53f754fa0aadd52ad1f9c8cd27acdc
Jewel James <jeweljames11@gmail.com>
2018-07-11 13:26:03 +0530

    Download working

--- main.go
+++ main.go
@@ -1,24 +1,42 @@
 package main
 
-import "fmt"
-import "github.com/turnage/graw/reddit"
+/*
+	1. Imgur Integration
+	2. Wallpaper Setting
+	3. Debugging
+*/
 
-var loc = "/home/pro/Dropbox/Code/golang/snoowall/"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/turnage/graw/reddit"
+)
+
+var loc = "/home/pro/Dropbox/Code/golang/snoowall/Wallpapers/"
 var name = "info.agent"
 var path = fmt.Sprintf("%s%s", loc, name)
-var subreddit = "/r/EarthPorn"
+var subreddit = "MapPorn"
+
+func saveWall(b []byte) {
+	timestamp := time.Now()
+	filename := fmt.Sprintf("%s%s_%s.jpg", loc, subreddit, timestamp.Format("2006-01-02_15-04-05"))
+	fmt.Println("Saving ...")
+	ioutil.WriteFile(filename, b, 0600)
+}
 
 func main() {
-	fmt.Println("Hello Snoowall!")
-	fmt.Println(path)
-	bot, err := reddit.NewBotFromAgentFile(path, 0)
-	fmt.Println(bot, err)
-	harvest, err := bot.Listing(subreddit, "")
-	fmt.Println(harvest, err)
-	if err != nil {
-		fmt.Println("Failed to fetch /r/golang: ", err)
-	}
-	for _, post := range harvest.Posts[:5] {
-		fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
-	}
+	rand.Seed(time.Now().UTC().UnixNano())
+	rate := 5 * time.Second
+	script, _ := reddit.NewScript("graw:doc_script:0.3.1 by /u/psychemerchant", rate)
+	harvest, _ := script.Listing(fmt.Sprintf("/r/%s", subreddit), "")
+	post := harvest.Posts[rand.Intn(30)]
+	fmt.Printf("[Title]: %s\n[URL]: %s\n", post.Title, post.URL)
+	resp, _ := http.Get(post.URL)
+	body, _ := ioutil.ReadAll(resp.Body)
+	saveWall(body)
+
 }
