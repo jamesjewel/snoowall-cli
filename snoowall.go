4fad7cd9c82faf734cdfbcd43d3417104014c9b1
Jewel James <jeweljames11@gmail.com>
2018-07-22 09:54:12 +0530

    setup logging

--- snoowall.go
+++ snoowall.go
@@ -4,14 +4,17 @@ package main
 	1. Imgur Integration (Done)
 	2. Wallpaper Setting (Done)
 	3. Debugging (Done)
-	4. Optimization: Switch to indexed Lurker implementation, Change file name to Name (Done), Bring binary data files
+	4. Optimization: Switch to indexed Lurker implementation
+						  Change file name to Name (Done)
+						  Bring binary data files
 	5. Command Line Options (Done)
 	6. Logging
 	7. Configuration File
 	8. PNG Problem (Done)
-	9. NSFW
-	.
-	.
+	9. NSFW (Done)
+	10. Develop Syncing
+	.			fmt.Printf("%s: File not found.  Creating new file.\n", fname)
+
 	NaN. Graphical User Interface
 */
 
@@ -19,21 +22,22 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/reujab/wallpaper"
 	"github.com/turnage/graw/reddit"
 )
 
-var loc = "/home/pro/Dropbox/Code/golang/snoowall/wallpapers/"
-var datafile = "data"
-var name = "info.agent"
-var path = fmt.Sprintf("%s%s", loc, name)
+var loc string
 var index int
 var subreddit string
 var top, nsfw bool
+
+var logfile = "logfile.log"
 
 func saveWall(filename string, b []byte) error {
 	err := ioutil.WriteFile(filename, b, 0600)
@@ -53,6 +57,10 @@ func setWall(file string) error {
 	return err
 }
 
+func sync() {
+
+}
+
 /*
 	The main code
 */
@@ -66,36 +74,48 @@ func main() {
 	flag.Parse()
 	fmt.Printf("[DEBUG] Arguments: sub:%s;  top:%t;  index:%d;  allow-nsfw:%t;  tail:%v\n", subreddit, top, index, nsfw, flag.Args())
 
+	f, err := os.OpenFile("LOG.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+
 	rate := 5 * time.Second
 	script, err := reddit.NewScript("graw:snoowall:0.3.1 by /u/psychemerchant", rate)
 	if err != nil {
-		fmt.Println("[DEBUG] Failed to create script handle: ", err)
+		log.Fatalln("[DEBUG] Failed to create script handle: ", err)
 		return
 	}
 
 	harvest, err := script.Listing(fmt.Sprintf("/r/%s", subreddit), "")
 	if err != nil {
-		fmt.Printf("[DEBUG] Failed to fetch /r/%s: %s", subreddit, err)
+		log.Fatalf("[DEBUG] Failed to fetch /r/%s: %s", subreddit, err)
 		return
 	}
 
 	var post *reddit.Post
+
 retry:
 	if top == true {
 		post = harvest.Posts[index]
 	} else if top == false {
-		post = harvest.Posts[rand.Intn(99)]
+		length := len(harvest.Posts)
+		if length == 0 {
+			log.Println("[DEBUG]: No posts! Subreddit might not exist.")
+			return
+		}
+		post = harvest.Posts[rand.Intn(length)]
 	}
 	if nsfw == false {
 		if post.NSFW == true {
-			fmt.Println("[DEBUG] Post is NSFW")
+			log.Println("[DEBUG] Post is NSFW")
 			if top == false {
 				goto retry
 			}
 		}
 	}
 
-	fmt.Println("[DEBUG] Post array length: ", len(harvest.Posts))
 	// postPermalinks := make([]string, 0, 100)
 	// for i := 0; i < 100; i++ {
 	// 	post := harvest.Posts[i]
@@ -105,29 +125,31 @@ retry:
 
 	// ioutil.WriteFile(datafile, []byte(post.Name), 0600)
 	// fmt.Println("After:", post.Name)
+
+	fmt.Println("[DEBUG] Post array length: ", len(harvest.Posts))
 	fmt.Printf("[Title]: %s\n[URL]: %s\n", post.Title, post.URL)
 
 	resp, err := http.Get(post.URL)
 	filetype := post.URL[len(post.URL)-4:]
 	if filetype != ".jpg" && filetype != ".png" {
-		fmt.Println("[DEBUG] Not an image.")
+		log.Println("[DEBUG] Not an image.")
 		goto retry
 	}
 	fmt.Println("[DEBUG] Image Type:", filetype)
 	if err != nil {
-		fmt.Println("[DEBUG]: Couldn't fetch resource:", post.URL, err)
+		log.Println("[DEBUG]: Couldn't fetch resource:", post.URL, err)
 		return
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
-	filename := fmt.Sprintf("%s%s_%s.%s", loc, subreddit, post.ID, filetype)
+	loc := fmt.Sprintf("%s/%s", os.Getenv("HOME"), "/Pictures/Wallpapers/")
+	filename := fmt.Sprintf("%s%s_%s%s", loc, subreddit, post.ID, filetype)
 	err = saveWall(filename, body)
 	if err != nil {
-		fmt.Println("[DEBUG] Wallpaper saving error:", err)
+		log.Println("[DEBUG] Wallpaper saving error:", err)
 	}
 	err = setWall(filename)
 	if err != nil {
-		fmt.Println("[DEBUG] Wallpaper setting error:", err)
+		log.Println("[DEBUG] Wallpaper setting error:", err)
 		return
 	}
-
 }
